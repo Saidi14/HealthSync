0948ceebf7e064995142f07af0b7e77270a8b2f3
naqeebah014 <naqeebahkhan18@gmail.com>
2025-08-10 20:20:15 +0200

    Backend setup

---
+++ backend/src/main/java/healthsync/domain/enums/ActivityLevel.java
@@ -0,0 +1,9 @@
+package healthsync.domain.enums;
+
+public enum ActivityLevel {
+    SEDENTARY("Sedentary"),
+    LIGHTLY_ACTIVE("Lightly Active"),
+    MODERATELY_ACTIVE("Moderately Active"),
+    VERY_ACTIVE("Very Active"),
+    EXTREMELY_ACTIVE("Extremely Active");
+}
